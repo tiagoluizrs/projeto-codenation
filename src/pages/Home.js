a7f1721422206e10a764e2bed405a3a0de6a56ed
tiagoribeirocb <tiago.ribeiro@codebuddy.com.br>
2020-04-20 15:03:51 -0300

    - SEO

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import * as ProductActions from "../data/actions/ProductActions";
 import {connect} from "react-redux";
 import Preloader from "../components/Preloader";
 import ReactGA from 'react-ga';
+import {Helmet} from 'react-helmet'
 
 class Home extends Component{
     static defaultProps = {
@@ -29,10 +30,28 @@ class Home extends Component{
     render(){
         const { props } = this;
         return(
-            props.productList.length === 0 ? <Preloader />:
-                <div>
+            <div>
+                <Helmet>
+                    <meta charSet="utf-8" />
+                    <title>Fashionista - Home</title>
+                    <meta name="robots" content="follow, index"/>
+                    <meta name="description" content="Loja virtual de teste para demonstrar os recursos do ReactJS."/>
+                    <meta property="og:locale" content="pt_BR"/>
+                    <meta property="og:url" content={window.location.pathname + window.location.search}/>
+                    <meta property="og:title" content="Fashionista - Home"/>
+                    <meta property="og:site_name" content="Fashionista"/>
+                    <meta property="og:description" content="Loja virtual de teste para demonstrar os recursos do ReactJS."/>
+                    <meta property="og:image" content={ window.location.pathname + "/static/media/banner.jpg"} />
+                    <meta property="og:image:type" content="image/jpeg"/>
+                    <meta property="og:image:width" content="800"/>
+                    <meta property="og:image:height" content="600"/>
+                    <meta property="og:type" content="website"/>
+                </Helmet>
+                {
+                    props.productList.length === 0 ? <Preloader />:
                     <ProductsList productList={props.productList}/>
-                </div>
+                }
+            </div>
         )
     }
 }
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -5,6 +5,7 @@ import ProductPage from "../components/product/ProductPage";
 import { Channel } from "../data/services/EventEmitter";
 import Preloader from "../components/Preloader";
 import ReactGA from 'react-ga';
+import {Helmet} from 'react-helmet'
 
 class Product extends Component{
     constructor(props){
@@ -71,11 +72,29 @@ class Product extends Component{
         const { props } = this;
 
         return(
-            Object.keys(props.productItem).length === 0 ? <Preloader />:
-            <ProductPage
-                addToCart={this.addToCart}
-                changeSize={this.changeSize}
-                productItem={props.productItem}/>
+                Object.keys(props.productItem).length === 0 ? <Preloader />:
+                <div>
+                    <Helmet>
+                        <meta charSet="utf-8" />
+                        <title>Fashionista - {props.productItem.name}</title>
+                        <meta name="robots" content="follow, index"/>
+                        <meta name="description" content="Loja virtual de teste para demonstrar os recursos do ReactJS."/>
+                        <meta property="og:locale" content="pt_BR"/>
+                        <meta property="og:url" content={window.location.pathname + window.location.search}/>
+                        <meta property="og:title" content={"Fashionista - " + props.productItem.name}/>
+                        <meta property="og:site_name" content="Fashionista"/>
+                        <meta property="og:description" content="Loja virtual de teste para demonstrar os recursos do ReactJS."/>
+                        <meta property="og:image" content={props.productItem.image} />
+                        <meta property="og:image:type" content="image/jpeg"/>
+                        <meta property="og:image:width" content="330"/>
+                        <meta property="og:image:height" content="420"/>
+                        <meta property="og:type" content="website"/>
+                    </Helmet>
+                    <ProductPage
+                        addToCart={this.addToCart}
+                        changeSize={this.changeSize}
+                        productItem={props.productItem}/>
+                </div>
         )
     }
 }
